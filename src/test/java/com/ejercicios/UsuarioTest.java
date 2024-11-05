065108be05001d155e4ee41972b78e556f39908c
Muzorn <jesusmgc.it@gmail.com>
2024-11-05 10:48:11 +0100

    Creamos un par de tests con las anotaciones de before y after.

--- src/test/java/com/ejercicios/UsuarioTest.java
+++ src/test/java/com/ejercicios/UsuarioTest.java
@@ -1,5 +1,8 @@
 package com.ejercicios;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,18 +11,27 @@ import static org.junit.jupiter.api.Assertions.*;
  * The type Usuario test.
  */
 class UsuarioTest {
+    private static Usuario usuario;
 
-    /**
-     * Login.
-     */
-    @Test
-    void login() {
+    @BeforeAll
+    static void setUsuario() {
+        UsuarioTest.usuario = new Usuario("John Doe", "Coco");
     }
 
-    /**
-     * Logout.
-     */
+    @BeforeEach
+    void setUp() {
+        System.out.println("Iniciando sesión...");
+        usuario.login();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Cerrando sesión...");
+        usuario.logout();
+    }
+
     @Test
-    void logout() {
+    void testPerfil() {
+        System.out.println("Perfil actualizado");
     }
 }
