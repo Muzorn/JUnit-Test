098c3200808c95806f1c2949015e4e37dd56996f
Muzorn <jesusmgc.it@gmail.com>
2024-11-05 10:34:32 +0100

    Creamos la clase de test para Usuario con dos métodos vacíos iniciales.

---
+++ src/test/java/com/ejercicios/UsuarioTest.java
@@ -0,0 +1,25 @@
+package com.ejercicios;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * The type Usuario test.
+ */
+class UsuarioTest {
+
+    /**
+     * Login.
+     */
+    @Test
+    void login() {
+    }
+
+    /**
+     * Logout.
+     */
+    @Test
+    void logout() {
+    }
+}
