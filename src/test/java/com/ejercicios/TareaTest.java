f6e89d88a1fbc684ae783e0bd51b6ccab70c01a6
Muzorn <jesusmgc.it@gmail.com>
2024-11-05 11:36:53 +0100

    Creamos un test para comprobar el número de elementos de la lista.

--- src/test/java/com/ejercicios/TareaTest.java
+++ src/test/java/com/ejercicios/TareaTest.java
@@ -1,12 +1,34 @@
 package com.ejercicios;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Tarea test.
+ */
 class TareaTest {
+    private static List<Tarea> tareas;
 
+    /**
+     * Sets up.
+     */
     @BeforeEach
     void setUp() {
+        TareaTest.tareas = new ArrayList<>();
+        tareas.add(new Tarea("Comprar pan", false));
+        tareas.add(new Tarea("Pagar factura", true));
+    }
+
+    /**
+     * Num elementos.
+     */
+    @Test
+    void numElementos() {
+        assertEquals(TareaTest.tareas.size(), 2);
     }
 }
