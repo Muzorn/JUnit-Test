de58f23853c94eede16f43171ea5443fb2cb23c4
Muzorn <jesusmgc.it@gmail.com>
2024-11-05 09:53:25 +0100

    Creamos un primer test de prueba que compruebe un resultado procedente de una suma.

--- src/test/java/com/springdemo/JUnitTestCaseTest.java
+++ src/test/java/com/springdemo/JUnitTestCaseTest.java
@@ -1,4 +1,12 @@
+package com.springdemo;
+
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class JUnitTestCaseTest {
-  
+    @Test
+    void miPrueba() {
+        int resultado = 2 + 2;
+        assertEquals(resultado, 4);
+    }
 }
