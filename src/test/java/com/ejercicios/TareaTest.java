ce171a6d722a4544d58c1786f5f47d847d98f72d
Muzorn <jesusmgc.it@gmail.com>
2024-11-05 11:31:10 +0100

    Generamos la clase de test para las pruebas sobre la clase Tarea.

---
+++ src/test/java/com/ejercicios/TareaTest.java
@@ -0,0 +1,12 @@
+package com.ejercicios;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TareaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
